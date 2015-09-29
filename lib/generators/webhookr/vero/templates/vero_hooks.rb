997f32447422c2ba09b82eb24e9b27cdc6833bfe
Kevin Smyth <kevin@tailorbrads.com>
2015-09-29 18:48:41 -0400

    Grab user info from payload

--- lib/generators/webhookr/vero/templates/vero_hooks.rb
+++ lib/generators/webhookr/vero/templates/vero_hooks.rb
@@ -5,37 +5,37 @@ class VeroHooks
   #
 
   def on_sent(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("Email sent: (#{user})")
   end
 
   def on_delivered(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("Email deliverd: (#{user})")
   end
 
   def on_opened(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("Email opened: (#{user})")
   end
 
   def on_clicked(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("Email clicked: (#{user})")
   end
 
   def on_bounced(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("Email bounced: (#{user})")
   end
 
   def on_unsubscribed(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("User unsubscribed: (#{user})")
   end
 
   def on_user_updated(incoming)
-    user = incoming.user
+    user = incoming.payload.user
     puts("User updated: (#{user})")
   end
 end
